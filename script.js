f75b54715c48bff4e32951d49232b84d4c160416
Anuragkolhe <128228032+Anuragkolhe@users.noreply.github.com>
2023-05-24 13:00:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
+// function volume_sphere() {
+//     //Write your code here
+//   const radius = document.getElementById("radius").value;
+//   const vol = (4 / 3) * Math.PI * Math.pow(radius, 3);
+//   document.getElementById("volume").value = vol;
+// } 
+
+// window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+
+
 function volume_sphere() {
-    //Write your code here
-  const radius = document.getElementById("radius").value;
-  const vol = (4 / 3) * Math.PI * Math.pow(radius, 3);
-  document.getElementById("volume").value = vol;
-} 
+  var radius = document.getElementById("radius").value;
+  var volume = (4/3) * Math.PI * Math.pow(radius, 3);
+  document.getElementById("volume").value = volume.toFixed(2);
+}
+
+document.getElementById("MyForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+  volume_sphere();
+});
+
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
